fa32d41d7ec7ffaf5ab99f5affd10da3cf7f6244
Galaxomas <thefidanabdulla@gmail.com>
2023-01-12 11:51:55 +0400

    unity prototype 1 scripts

---
+++ Scripts/FollowPlayer1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer1 : MonoBehaviour
+{
+    // main vehicles player
+    public GameObject player;
+
+    // offsets of camera positions
+    private Vector3 offset = new Vector3(0, 5, -7);
+    private Vector3 offset2 = new Vector3(0, 2, 1);
+
+    // camera switch position value
+    private bool switchPosition = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("v"))
+        {
+            switchPosition = !switchPosition;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (switchPosition)
+        {
+            transform.position = player.transform.position + offset2;
+        }
+        else
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
